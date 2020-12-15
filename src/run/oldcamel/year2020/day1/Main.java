73c92f568de73ff32008121439b377d5eb529929
xucheng <xucheng@yunzainfo.com>
2020-12-15 10:19:08 +0800

    add doc

--- src/run/oldcamel/year2020/day1/Main.java
+++ src/run/oldcamel/year2020/day1/Main.java
@@ -4,7 +4,24 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 
-public class Part1 {
+
+/**
+ * Specifically, they need you to find the two entries that sum to 2020 and then multiply those two numbers together.
+ *
+ * For example, suppose your expense report contained the following:
+ *
+ * 1721
+ * 979
+ * 366
+ * 299
+ * 675
+ * 1456
+ * In this list, the two entries that sum to 2020 are 1721 and 299. Multiplying them together produces 1721 * 299 = 514579, so the correct answer is 514579.
+ *
+ * Of course, your expense report is much larger. Find the two entries that sum to 2020; what do you get if you multiply them together?
+ *
+ * */
+public class Main {
 
     public static void main(String[] args) {
         int[] arr = {1973,
