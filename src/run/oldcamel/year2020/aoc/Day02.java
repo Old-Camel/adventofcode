a954ae2a8b4d35d979a08c2e086a98d3b12ddba6
xucheng <xucheng@yunzainfo.com>
2020-12-15 14:33:48 +0800

    重构

---
+++ src/run/oldcamel/year2020/aoc/Day02.java
@@ -0,0 +1,13 @@
+package run.oldcamel.year2020.aoc;
+
+/**
+ * Created by IntelliJ IDEA
+ * TODO: TODO
+ *
+ * @author: 徐成
+ * Date: 2020/12/15
+ * Time: 2:26 下午
+ * Email: old_camel@163.com
+ */
+public class Day02 {
+}
