d8f8f251ed6cd0371fc663e7bd3caa17c73bc963
xucheng <xucheng@yunzainfo.com>
2020-12-16 11:11:28 +0800

    day04 part02

--- src/run/oldcamel/year2020/aoc/Day04.java
+++ src/run/oldcamel/year2020/aoc/Day04.java
@@ -58,23 +58,103 @@ public class Day04 {
 
             for (String key : keys) {
                 if (!datum.containsKey(key)) {
-                    break;
+                    continue a;
                 }
-                if (key.equals(keys[keys.length - 1])) {
-                    flag++;
-                }
+
+            }
+            flag++;
+        }
+        return flag;
+    }
+
+    public long part2() {
+        long flag = 0;
+        a:
+        for (HashMap<String, String> datum : data) {
+            if(byr(datum)&&iyr(datum)&&eyr(datum)&&hgt(datum)&&hcl(datum)&&ecl(datum)&&pid(datum)){
+                flag++;
+
             }
 
         }
         return flag;
     }
 
-    public long part2() {
-        return -1;
+    private boolean byr(HashMap<String, String> map) {
+        String byr = map.get("byr");
+        try{
+            int i = Integer.parseInt(byr);
+            if(1920<=i&&i<=2002){
+                return true;
+            }
+        }catch (Exception e){
+            return  false;
+        }
+        return false;
+    }
+    private boolean iyr(HashMap<String, String> map) {
+        String byr = map.get("iyr");
+        try{
+            int i = Integer.parseInt(byr);
+            if(2010<=i&&i<=2020){
+                return true;
+            }
+        }catch (Exception e){
+            return  false;
+        }
+        return false;
+    }
+    private boolean eyr(HashMap<String, String> map) {
+        String byr = map.get("eyr");
+        try{
+            int i = Integer.parseInt(byr);
+            if(2020<=i&&i<=2030){
+                return true;
+            }
+        }catch (Exception e){
+            return  false;
+        }
+        return false;
+    }
+    private boolean hgt(HashMap<String, String> map) {
+        String hgt = map.get("hgt");
+        try{
+           if(hgt.endsWith("cm")){
+               String cm = hgt.replaceAll("cm", "");
+               int i = Integer.parseInt(cm);
+               if(150<=i&&i<=193){
+                   return true;
+               }
+           }else if(hgt.endsWith("in")){
+               String cm = hgt.replaceAll("in", "");
+               int i = Integer.parseInt(cm);
+               if(59<=i&&i<=76){
+                   return true;
+               }
+           }
+        }catch (Exception e){
+            return  false;
+        }
+        return false;
+    }
+    private boolean hcl(HashMap<String, String> map) {
+
+        String hgt = map.get("hcl");
+        return hgt!=null&& hgt.matches("#[0-9a-f]{6}");
+    }
+    private boolean ecl(HashMap<String, String> map) {
+        String ecl = map.get("ecl");
+        String all="amb blu brn gry grn hzl oth";
+        return ecl!=null&&all.contains(ecl);
+    }
+    private boolean pid(HashMap<String, String> map) {
+        String pid = map.get("pid");
+
+        return pid!=null&&pid.matches("[0-9]{9}");
     }
 
-
     public static void main(String[] args) {
+        //System.out.println("1#333222".matches("a#[0-9a-f]{6}"));
         Day04 day02 = new Day04(new File("src/file/day04.txt"));
         System.out.println(day02.part1());
         System.out.println(day02.part2());
